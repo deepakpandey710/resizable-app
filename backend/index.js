ae9afd33dd1dbc548de04c904de0aa9198c9686e
Deepak pandey <pandeydeepak710@gmail.com>
2024-04-03 11:04:30 +0530

    vercel backend setup

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 mongoose.connect('mongodb+srv://deepakpandey7100:tk3Vaq6BVgbi0EiZ@cluster0.ldvrfga.mongodb.net/dataNeuron?retryWrites=true&w=majority&appName=Cluster0')
 .then(() => console.log("Database connected"));
 app.use('/api/user', userRoutes);
-
+app.get('/',(req,res)=>{res.send('hello')});
 app.listen(5000, ()=>{
     console.log("Server started at port 5000")}
 )
